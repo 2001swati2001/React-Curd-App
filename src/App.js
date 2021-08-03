d63daeba0bf9c23f2dd572712f8a7f28438c9a2c
2001swati2001 <swatilaxmisahu@gmail.com>
2021-08-04 01:05:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home.js";
+import Navbar from "./components/Layout/Navbar.js";
+import {BrowserRouter,Switch,Route} from "react-router-dom";
+import AddUser from './components/userdata/AddUser';
+import EditUser from './components/userdata/EditUser';
+import ViewUser from './components/userdata/ViewUser';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <BrowserRouter>
+        <Navbar />
+          <Switch>
+            
+            <Route exact path="/"> 
+              <Home /> 
+            </Route>
+
+            <Route exact path="/users/add"> 
+              <AddUser /> 
+            </Route>
+
+            <Route exact path="/users/edit/:id"> 
+              <EditUser /> 
+            </Route>
+
+            <Route exact path="/users/view/:id"> 
+              <ViewUser /> 
+            </Route>
+
+          </Switch>
+      </BrowserRouter>
+        
+    </div>
+  );
+}
+
+export default App;
